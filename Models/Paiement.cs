99b66e8eaef1253f130c86bb7e0900917a00cf31
TORINO <victorinattolode@gmail.com>
2024-11-15 08:12:17 +0000

    Cours-3_4

---
+++ Models/Paiement.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Cours.Models;
+
+public class Paiement
+{
+
+
+  public int Id { get; set; }
+  public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+  [Range(1, float.MaxValue, ErrorMessage = "Seul un montant positif est autorisé")]
+  public float Montant { get; set; }
+
+
+  // Relation
+  public Dette Dette { get; set; }
+  public int DetteId { get; set; }
+
+
+
+
+
+
+
+
+
+}
